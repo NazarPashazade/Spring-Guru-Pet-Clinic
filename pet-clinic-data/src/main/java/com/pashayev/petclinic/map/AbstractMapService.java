58fb7211756c03f82d7af5ab1abf23c1b1d01ce8
NezerPashazade <53702447+NezerPashazade@users.noreply.github.com>
2020-07-02 11:40:09 +0400

    create 'AbstractMapServices' Abstract class

---
+++ pet-clinic-data/src/main/java/com/pashayev/petclinic/map/AbstractMapService.java
@@ -0,0 +1,35 @@
+package com.pashayev.petclinic.map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> {
+
+    public Map<ID,T>  map=new HashMap<>();
+
+
+    public Set<T>findAll(){
+        return new HashSet<>(map.values());
+    }
+
+    public T finById(ID id){
+        return map.get(id);
+    }
+
+    public T save(ID id, T object){
+      map.put(id,object);
+      return object;
+    }
+
+    public void delete(ID id){
+        map.entrySet().removeIf(entry->entry.getKey().equals(id));
+    }
+
+    public void deleteById(T object){
+        map.entrySet().removeIf(entry->entry.getValue().equals(object));
+    }
+
+
+}
